b1630e29f63def3654cc32ff9e0e66c584d50c4a
Stanislav Posidelov <stanisPos@gmail.com>
2020-01-12 13:53:15 +0300

    Added redux, create store and first actions with reducers for currency list

--- src/ducks/currency/sagas.ts
+++ src/ducks/currency/sagas.ts
@@ -1,6 +1,6 @@
 import { all } from 'redux-saga/effects';
 import { processSagas } from '../../helpers/sagas-helpers';
-import { GET_CURRENCIES_LIST, UPDATE_CURRENCIES_LIST } from './action-types';
+import { GET_CURRENCIES_LIST, SET_BASE_CURRENCY, UPDATE_CURRENCIES_LIST } from './action-types';
 
 const sagas = {
     * [GET_CURRENCIES_LIST](action: any) {
@@ -9,6 +9,11 @@ const sagas = {
     },
 
     * [UPDATE_CURRENCIES_LIST](action: any) {
+        console.log(action);
+        return yield 2;
+    },
+
+    * [SET_BASE_CURRENCY](action: any) {
         console.log(action);
         return yield 2;
     }
