2ae8e41925c5e93ba8bcdf1b364a15166cf78e34
Stanislav Posidelov <sposidelov@roox.ru>
2021-06-13 22:07:27 +0300

    dev configurated

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
-import './App.css';
+import * as styles from './App.css';
 
 function App() {
+  console.log(styles)
   return (
     <div className="App">
       <header className="App-header">
